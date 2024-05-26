a5cd2ff55efc74167e865f14ecfb053c913a1edb
123naim <mdnayemiqbal886@gmail.com>
2024-05-26 23:40:32 +0600

    strict added

--- Chapter-8/strict.js
+++ Chapter-8/strict.js
@@ -1,1 +1,20 @@
-const hello = 'hello world'
+// "use strict";
+// let x;
+// x = 32;
+// console.log(x)
+
+// let y;
+// y = 3;
+// console.log(y)
+
+// "use strict"
+x = 4;       // This will not cause an error.
+myFunction();
+
+function myFunction() {
+//   "use strict";
+  let y;
+  y = 3.14;   // This will cause an error
+  console.log(y)
+}
+console.log(x)
