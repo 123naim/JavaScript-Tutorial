ffd34eb250cbcad41590cd7c3445778642d0c30e
123naim <mdnayemiqbal886@gmail.com>
2024-05-27 22:25:41 +0600

    reduce use kore sum nirnoi

--- Chapter-8/Iteration/map.js
+++ Chapter-8/Iteration/map.js
@@ -3,3 +3,15 @@ const sum = arr.map(v => {
     return v * v * v;
 })
 console.log(sum)
+
+
+const numbers = [1, 2, 3, 4, 5];
+// const doubledNumbers = numbers.map(number => number * 2);
+const doubledNumbers = numbers.map(numbers => numbers * 2);
+console.log(doubledNumbers)
+
+const sum2 = numbers.reduce((accumulator, value) => accumulator + value);
+console.log(sum2)
+
+const sum3 = doubledNumbers.reduce((acc, value) => acc + value, 0)
+console.log(sum3)
