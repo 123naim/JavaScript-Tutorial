22bfa1acc482fece2321d85b3a7d78fd5c538a8b
123naim <mdnayemiqbal886@gmail.com>
2024-05-27 22:48:32 +0600

    for git added

---
+++ Chapter-8/for.js
@@ -0,0 +1,17 @@
+const arr = [1, 2, false, null, 3, 5, 6, '', 'string', 9, 6];
+
+let count = 0;
+for(let i = 0; i< arr.length; i++){
+    for(let j = i; j < arr.length - 1; j++){
+        if(!arr[j] || typeof arr[j] !== 'number'){
+            arr[j] = arr[j + 1];
+            arr[j + 1] = undefined;
+        }
+        0
+    }
+    if(typeof arr[i] === 'undefined'){
+        count++; 
+    }
+}
+arr.length -= count;
+console.log(arr)
