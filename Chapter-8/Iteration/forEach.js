cd5b7694af23ebdd46cf2c178af0160bec45fe32
123naim <mdnayemiqbal886@gmail.com>
2024-05-27 22:12:50 +0600

    for Each defferant sum added

--- Chapter-8/Iteration/forEach.js
+++ Chapter-8/Iteration/forEach.js
@@ -2,3 +2,11 @@ const arr = [1, 3, 4, 5, 7, 8]
 let sum = 0;
 arr.forEach(v => sum += v)
 console.log(sum)
+
+const arr2 = [3, 5, 2, 5, 7, 9];
+let sum2 = 0;
+arr2.forEach(function(v){
+    sum2 += v;
+})
+
+console.log(sum2)
