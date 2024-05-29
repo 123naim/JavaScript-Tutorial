219b6bdf74d5c7f32016544162baa32517e06012
123naim <mdnayemiqbal886@gmail.com>
2024-05-29 22:31:44 +0600

    map methode dive drive

--- Chapter-8/Iteration/map.js
+++ Chapter-8/Iteration/map.js
@@ -1,17 +1,19 @@
-const arr = [1, 3, 4, 5, 7, 8];
-const sum = arr.map(v => {
-    return v * v * v;
+// 2. The map() method:
+// This method gets an array and then transforms the elements of that array as its function pleases and then creates a new array and stores them all in the new array. Imagine we have an array of numbers, and for some reason, we want to double up all of its elements; we can use the forEach loop to get all the elements and then double them up, but we can also use the map() method as well.
+
+// So let’s recheck the oldArray and see how we can work with the map() method:
+
+
+const oldArray = [10, 20, 30, 40, 50];
+const newArray = oldArray.map(function(number){
+    return number * 2;
 })
-console.log(sum)
+console.log(newArray)
 
 
-const numbers = [1, 2, 3, 4, 5];
-// const doubledNumbers = numbers.map(number => number * 2);
-const doubledNumbers = numbers.map(numbers => numbers * 2);
-console.log(doubledNumbers)
+// ES6
+const newArray2 = oldArray.map(number => number * 2)
+console.log(newArray2)
 
-const sum2 = numbers.reduce((accumulator, value) => accumulator + value);
-console.log(sum2)
 
-const sum3 = doubledNumbers.reduce((acc, value) => acc + value, 0)
-console.log(sum3)
+// The important thing about the map() method compared with forEach is that the forEach loop does not actually return anything and also changes the original array. In contrast, the map() method creates a new array.
